8612fb8229d3c438706fc0f992cb35a91e54d765
JavohirBerdiyev <javohir3324497@gmail.com>
2021-07-15 23:50:55 +0500

    initial commit

---
+++ src/Books.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Books = () => {
+  return (
+    <div>
+      <h1>Book list</h1>
+      <ul>
+        <li>Book1</li>
+        <li>Book2</li>
+        <li>Book3</li>
+        <li>Book4</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Books;
